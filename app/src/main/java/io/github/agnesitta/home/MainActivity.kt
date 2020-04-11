2818b44442b53cc1dc4d4f3cb1c6e0736b01b8ef
Agnieszka Czerwinska <agnesitta@gmail.com>
2020-04-11 13:25:58 +0100

    Basic Project Setup

---
+++ app/src/main/java/io/github/agnesitta/home/MainActivity.kt
@@ -0,0 +1,18 @@
+package io.github.agnesitta.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.agnesitta.home.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
